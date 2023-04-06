00a91b825d6844728becc12b2c806296f0fc51e9
Mar2102 <129897990+Mar2102@users.noreply.github.com>
2023-04-06 11:09:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,7 +7,8 @@ from scipy.stats import norm
 chat_id = 682673597 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    alpha=1-p
     size = x.size
     max = x.max()
-    return max/(p**(1./size)), \
-           max/((1-p)**(1./size))
+    return max/((1-alpha/2)**(1./size)), \
+           max/(alpha/2**(1./size))
